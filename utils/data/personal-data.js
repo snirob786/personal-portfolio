5025fa5e5bb076da97bd2ffef46e4d08f6d868ce
Saifur Rahman Nirob <snirob786@gmail.com>
2025-06-07 19:40:35 +0600

    feat: initiated the portfolio

---
+++ utils/data/personal-data.js
@@ -0,0 +1,19 @@
+export const personalData = {
+  name: "S. R. Nirob",
+  profile: "/profile.png",
+  designation: "Fullstack Developer",
+  description:
+    "My name is Md. Saifur Rahman Nirob. I am a passionate and professional programmer with a strong enthusiasm for continuous learning and problem-solving. With a self-driven attitude, I enjoy exploring new technologies and diving deep into challenging tasks. I specialize in full-stack web application development and love contributing to making the web more dynamic and accessible. My core strength lies in JavaScript, and I prefer building robust and scalable solutions using JavaScript-based technologies. I’m open to any job opportunity that aligns with my skills, interests, and values.",
+  email: "snirob786@gmail.com",
+  phone: "+8801714621004",
+  // address: "Middle Badda, Dhaka, Bangladesh - 1212 ",
+  github: "https://github.com/snirob786",
+  facebook: "https://www.facebook.com/theSRNOfiicial/",
+  linkedIn: "https://www.linkedin.com/in/snirob786/",
+  twitter: "", //"https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/10182184/saifur-rahman-nirob",
+  leetcode: "", //"https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume:
+    "https://drive.google.com/file/d/1YmT7KSmPS4IVbyWif1Ybct8vJXueCaxU/view?usp=sharing",
+};
